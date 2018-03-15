10ef314122c9a0a8e34a4abbc95ef76990f19855
Jesperpaulsen <37295027+Jesperpaulsen@users.noreply.github.com>
2018-03-15 11:10:10 +0100

    Update Okt.java

--- Okt.java
+++ Okt.java
@@ -1,44 +1,44 @@
 package treningsdagbok;
 
 import java.sql.*;
-import java.text.SimpleDateFormat;
+//import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Okt {
 
 	private Connection conn;
-	private int oktId, varighet;
-	private java.sql.Date dato;
-	private String informasjon;
+	private int oktId;
+	private String dato,varighet,tidspunkt,informasjon;
 
 	public Okt(Connection conn) {
 		this.conn = conn;
-		this.oktId = getOktIdFromDB(conn);
+		//this.oktId = getOktIdFromDB(conn);
 
 	}
 	public int getOktId() {return oktId;}
-	public int getVarighet(){return varighet;}
-	public java.sql.Date getDato() {return dato;}
+	public String getVarighet(){return varighet;}
+	public String getDato() {return dato;}
+	public String getTidspunkt() {return tidspunkt;}
 	public String getInformasjon() {return informasjon;}
 	
-	@SuppressWarnings("deprecation")
 	public void addOkt(Scanner scanner) throws SQLException {
-		System.out.println("Legg til treningsokt, skriv inn dato og tid i formatet yyyymmdd");
-		String date=scanner.nextLine();
-		dato = java.sql.Date.valueOf( date );
-		System.out.println("Hvor lenge varte okten i minutter?");
-		varighet=Integer.parseInt(scanner.nextLine());
+		System.out.println("Legg til treningsokt, skriv inn dato og tid i formatet yyyy-mm-dd");
+		dato=scanner.nextLine();
+		System.out.println("Hvilket tidspunkt trente du?");
+		tidspunkt=scanner.nextLine();
+		System.out.println("Hvor lenge varte okten?");
+		varighet=scanner.nextLine();
 		System.out.println("Informasjon om okten");
 		informasjon=scanner.nextLine();
 		
-	String oktSql = String.format("INSERT INTO okt VALUES(%d, %d, %d, '%s', '%s')", getOktId(),getDato(), getVarighet(), getInformasjon());
+	String oktSql = String.format("INSERT INTO okt(dato,tidspunkt,varighet,informasjon) VALUES('%s', '%s', '%s', '%s')",getDato(), getTidspunkt(),getVarighet(), getInformasjon());
 	
 	Statement stmt=conn.createStatement();
 	stmt.executeUpdate(oktSql);
 	
 	}
 	
-	public int getOktIdFromDB(Connection conn) {
+	/*public int getOktIdFromDB(Connection conn) {
 		String query = "SELECT oktId FROM okt ORDER BY oktID DESC LIMIT 1";
 		try {
 			ResultSet rs = getResultSet(conn, query);
@@ -56,5 +56,5 @@ public class Okt {
 	private static ResultSet getResultSet(Connection conn, String query) throws SQLException {
 		Statement stmt = conn.createStatement();
 		return stmt.executeQuery(query);
-	}
+	}*/
 }
